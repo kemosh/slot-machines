c619224b911513dc6ccce556fd9bacb39cb00ec7
Alessandro Della Villa <alessandro@axxs.sport>
2024-10-17 19:24:13 +0200

    mount points

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,16 @@ set -euo pipefail
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PROJECT_DIR="$(cd "$dir/.." && pwd)"
 
+# create pvs mount points
+for vfile in $PROJECT_DIR/services/*/volumes.yaml; do
+    dir=$(dirname "${vfile}")
+    service_name="$(basename "$dir")"
+    mkdir -p $PROJECT_DIR/pvs/$service_name
+    for mount_point in $(yq .volumes[] < $vfile); do
+        mkdir -pv "$PROJECT_DIR/pvs/$service_name/$mount_point"
+    done
+done
+
 # generate requirements
 for rin in $PROJECT_DIR/services/*/requirements.in; do
     dir=$(dirname "${rin}")
